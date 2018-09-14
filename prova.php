3de27e0da1d118eadcfdb1964094d3619f34e24d
bernardinigianluca <33350971+bernardinigianluca@users.noreply.github.com>
2018-09-14 11:06:44 +0200

    Create prova.php

---
+++ prova.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <h1>Prova</h1>
+    <?php
+      $test = "prova";
+      define("Const1","valore constante prova");
+      echo "Variabile php $test " . $test;
+      echo "<br>";
+      echo "Costante php Const1 " . Const1;
+    ?>
+  </body>
+</html>
