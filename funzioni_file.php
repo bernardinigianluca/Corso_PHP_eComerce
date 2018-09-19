7cc09ccd125c799111d12db04f3e2176e8808d82
bernardinigianluca <bernardinigianluca@hotmail.com>
2018-09-19 11:13:18 +0200

    Revert ""
    This reverts commit dcbc7595ce3ae66ac0700e819f68ac98fddd71b5.

--- funzioni_file.php
+++ funzioni_file.php
@@ -2,52 +2,20 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Gestione dei file</title>
+        <title>funzioni file</title>
     </head>
     <body>
-        <?php 
-            $miofile = fopen("mio_testo1.txt","w+");
-            fwrite($miofile, "Nuova riga") or die("Non posso scrivere nel file");
-            fclose($miofile)
-        ?>
-        <h1>Fopen / Fclose </h1>
+        <h1>file_put_contents</h1>
         <?php
+        $filename = "files/miotesto.txt";
+        $use_include_path = true;
         
-        if(file_exists("mio_testo1.txt")){
-            $miofile = fopen("mio_testo1.txt","w+");
-            fclose($miofile);
-            echo "File essistente, aperto e chiuso";
-        } else {
-            echo "File non essiste!";
-        }
-        ?>
-        
-        <h1>fread</h1>
-        <?php 
-            $gestione_file = fopen("mio_testo1.txt","w+") or die(
-                    "Impossibile aprire il file");
-            $contenuto_file = fread($gestione_file, filesize("mio_testo1.txt"));
-            fclose($gestione_file);
-            
-            echo $contenuto_file;
-        ?>
-        
-        <h1>file_get_contents</h1>
-        <?php 
-            $contenuto_file = file_get_contents("mio_testo1.txt") or die(
-                    "Impossibile accedere al contenuto");
-            
-            echo $contenuto_file;
-        ?>
-        
-        <h1>fwrite</h1>
-        <?php 
-            $miofile = fopen("mio_testo1.txt","w+");
-            fwrite($miofile, "Nuova riga") or die("Non posso scrivere nel file");
-            
-            $contenuto_file = file_get_contents("mio_testo1.txt");
-            
-            echo $contenuto_file;
+         file_put_contents($filename, " Questo è un altro contenuto",FILE_APPEND)
+                 or die("Mi dispiace, non è possibile scrivere nel file");
+         
+         $contenutofile = file_get_contents($filename, $use_include_path);
+         
+         echo $contenutofile;
         ?>
     </body>
 </html>
